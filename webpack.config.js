5a2a46b6c102b721ba67e74a65435243ee2f9817
Thanga Ganapathy <ganapathy888@gmail.com>
2021-05-19 15:08:07 +0530

    Init react app using webpack

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  target: 'electron-renderer',
+  entry: './src/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  devtool: 'eval-cheap-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Ruby REPL',
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
