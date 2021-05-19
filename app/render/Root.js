ffc8c5c5e72c4718aebfc7e22c1e3141cbbf4643
Thanga Ganapathy <ganapathy888@gmail.com>
2021-05-19 20:12:51 +0530

    Add material ui and create app layout

--- app/render/Root.js
+++ app/render/Root.js
@@ -1,3 +1,20 @@
+import { Box, Paper } from '@material-ui/core';
+
 export default function App() {
-  return <h1>React App!</h1>;
+  return (
+    <Paper
+      sx={{ height: '100%', padding: '15px', boxSizing: 'border-box' }}
+      square
+    >
+      <Box
+        sx={{ height: '100%', display: 'grid', gridTemplateRows: '50px 1fr' }}
+      >
+        <Box>Header</Box>
+        <Box sx={{ display: 'grid', gridTemplateColumns: '50% 50%' }}>
+          <Box>Editor</Box>
+          <Box>Output</Box>
+        </Box>
+      </Box>
+    </Paper>
+  );
 }
--- app/render/index.js
+++ app/render/index.js
@@ -1,4 +1,21 @@
 import ReactDOM from 'react-dom';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
 import Root from './Root';
 
-ReactDOM.render(<Root />, document.getElementById('container'));
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    divider: 'rgba(255,255,255,0.12)',
+  },
+});
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Root />
+    </ThemeProvider>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
