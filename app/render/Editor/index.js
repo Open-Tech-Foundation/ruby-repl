c38e13c4ee44c5bbcdad073056f66c1569ff4770
Thanga Ganapathy <ganapathy888@gmail.com>
2021-05-22 00:04:33 +0530

    Add editor onchange event handler after loading main file

--- app/render/Editor/index.js
+++ app/render/Editor/index.js
@@ -1,10 +1,11 @@
 import { Box } from '@material-ui/core';
 import * as monacoEditor from 'monaco-editor';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Editor() {
   const editorContainerRef = useRef(null);
   const editorRef = useRef(null);
+  const [loadingFile, setLoadingFile] = useState(true);
 
   useEffect(() => {
     editorRef.current = monacoEditor.editor.create(editorContainerRef.current, {
@@ -16,19 +17,17 @@ export default function Editor() {
   }, []);
 
   useEffect(() => {
-    console.log('editorRef: ', editorRef);
-    if (editorRef) {
-      editorRef.current.onDidChangeModelContent((e) => {
-        console.log(editorRef.current.getValue());
-        console.log(e);
+    if (editorRef.current && !loadingFile) {
+      editorRef.current.onDidChangeModelContent(() => {
         saveFile(editorRef.current.getValue());
       });
     }
-  }, [editorRef]);
+  }, [editorRef, loadingFile]);
 
   async function loadFileToEditor(filename) {
     const content = await window.electron.invoke('load-file', filename);
     editorRef.current.getModel().setValue(content);
+    setLoadingFile(false);
   }
 
   async function saveFile(content) {
