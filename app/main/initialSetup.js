553bc641fe7ffe5894b1e323c59eb1c691000350
Thanga Ganapathy <ganapathy888@gmail.com>
2021-05-21 19:27:13 +0530

    Create main file with default content in initial setup

--- app/main/initialSetup.js
+++ app/main/initialSetup.js
@@ -1,10 +1,22 @@
 const fs = require('fs');
+const path = require('path');
+
 const getUserFilesDir = require('./getUserFilesDir');
 
 function initialSetup() {
   const userFilesDir = getUserFilesDir();
   if (!fs.existsSync(userFilesDir)) {
     fs.mkdirSync(userFilesDir);
+  }
+
+  const mainFilePath = path.join(userFilesDir, 'main.rb');
+  if (!fs.existsSync(mainFilePath)) {
+    const content = "def msg\n  'I 😍 RUBY'\nend\n\nputs msg";
+    try {
+      fs.writeFileSync(mainFilePath, content);
+    } catch (err) {
+      console.error(err);
+    }
   }
 }
 
